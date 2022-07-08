3e19ee217e07d1885c91216db9c8d6564e577924
Webzerot <82109376+webzerot@users.noreply.github.com>
2022-07-08 16:57:10 +0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,149 +1,151 @@
 from pytube import YouTube
-from tkinter import *
-from threading import Thread
-import speedtest
-from time import sleep
-from sys import argv
-import signal
+import os
+import threading
 from os import _exit
-from os import system
-from os import path
+from tkinter import *
 
-# https://www.youtube.com/watch?v=rxGnonKB7TY ## sample video
+# sample video : https://www.youtube.com/watch?v=OQ6R4TU5Vwo
 
-videoLink = ""
-videoRes = ""
-pathToProgram = str( path.abspath(__file__) ).replace( argv[0],"" )
+APPLICATION_NAME = "youtube2MP3"
+LOGGING = True
 
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 300
-WINDOW_TITLE = "simple video downloader"
-PROGRAM_TITLE = "Youtube Video Downloader"
+WINDOW_TITLE = APPLICATION_NAME
 WINDOW_BACKGROUND_COLOR = "#DEBAEE"
 
-def bytes_to_mb(bytes):
-    KB = 1024 # One Kilobyte is 1024 bytes
-    MB = KB * 1024 # One MB is 1024 KB
-    return int(bytes/MB)
+# initializing window
+MAIN_WINDOW = Tk()
 
-def on_closing(Caller=""):
-    if (Caller == "console"):
-        _exit(1)
-    else:
-        window.destroy()
-        _exit(1)
+# CheckBox Var
+CheckBox_Var = IntVar()
 
-def OnExithandler(signum, frame):
-    _exit(1)
+class Status:
+    def IDLE():
+        return "Status : Idle"
+    def STARTING():
+        return "Status : Starting"
+    def DOWNLOADING():
+        return "Status : Downloading"
 
-internetSpeed = 0
+def LOG(value):
+    if LOGGING == True:
+        print(value)
 
-# window declaration
-try:
-    if (str(argv[1]).lower() == "console"):
-        pass
-except:
-    window = Tk()
+def GUI_PROPERTIES():
+    global MAIN_WINDOW
+    MAIN_WINDOW.title(WINDOW_TITLE)
+    MAIN_WINDOW.geometry(f"{str(WINDOW_WIDTH)}x{str(WINDOW_HEIGHT)}")
+    MAIN_WINDOW.configure(bg=WINDOW_BACKGROUND_COLOR)
+    MAIN_WINDOW.resizable(False,False)
 
-    #GUI
-    Text_TitleText = Label(font=("Elvetica",16),text=PROGRAM_TITLE,bg=WINDOW_BACKGROUND_COLOR)
-    Text_TitleText.place(x=70, y=10)
+#GUI ELEMENTS
 
-    Text_StatusText = Label(font=("Elvetica",16),text="Status : Idle",bg=WINDOW_BACKGROUND_COLOR)
-    #Text_StatusText.place(x=135, y=120)
-    Text_StatusText.place(x=10,y=260)
+Text_TitleText = Label(font=("Elvetica",16),text=APPLICATION_NAME,bg=WINDOW_BACKGROUND_COLOR)
+Text_TitleText.pack()
 
-    TextBox_VideoLink = Text(window, height = 1, width = 47,font=('Elvetica',11),bg="#ECE3C1")
-    TextBox_VideoLink.place(x=10,y=70)
+Text_VideoLink = Label(font=("Elvetica",13),text="Video Link",bg=WINDOW_BACKGROUND_COLOR)
+Text_VideoLink.pack()
 
-def MeasureInternetSpeed():
-    global internetSpeed
-    speed_test = speedtest.Speedtest()
-    download_speed = bytes_to_mb(speed_test.download())
-    internetSpeed = download_speed
+Text_VideoPath = Label(font=("Elvetica",13),text="File Path (blank for current)",bg=WINDOW_BACKGROUND_COLOR)
+Text_VideoPath.place(x=97,y=83)
 
-def multiThreadDownloadGUI():
-    Thread(target=DownloadVideoGUI).start()
+TextBox_youtube_VideoLink = Text(MAIN_WINDOW, height = 1, width = 47,font=('Elvetica',11),bg="#ECE3C1")
+TextBox_youtube_VideoLink.place(x=10,y=60)
 
-def InternetSpeedThread():
-    Thread(target=MeasureInternetSpeed).start()
+TextBox_VideoPath = Text(MAIN_WINDOW, height = 1, width = 47,font=('Elvetica',11),bg="#ECE3C1")
+TextBox_VideoPath.place(x=10,y=110)
 
-def DownloadVideoGUI():
-    global videoLink
-    videoLink = str(TextBox_VideoLink.get(1.0, "end-1c"))
+Text_StatusText = Label(font=("Elvetica",16),text=Status.IDLE(),bg=WINDOW_BACKGROUND_COLOR)
+Text_StatusText.place(x=10,y=260)
+
+CheckBox_Mp4_Download = Checkbutton(MAIN_WINDOW, text = "MP4",variable=CheckBox_Var,bg=WINDOW_BACKGROUND_COLOR)
+CheckBox_Mp4_Download.place(x=330,y=260)
+
+def Download_Vid_To_Mp3():
+    global TextBox_youtube_VideoLink
+    global Text_StatusText
+    global TextBox_VideoPath
+    try:
+        Text_StatusText.config(text=Status.STARTING())
+        m_VideoLink = str(TextBox_youtube_VideoLink.get(1.0, "end-1c"))
+        m_VideoPath = str(TextBox_VideoPath.get(1.0, "end-1c"))
+
+        yt = YouTube(m_VideoLink)
+
+        video = yt.streams.filter(only_audio=True).first()
+        # starting download
+        Text_StatusText.config(text=Status.DOWNLOADING())
+        output_file = video.download(m_VideoPath)
+
+        if (os.path.exists(str(output_file).replace(".mp4",".mp3"))):
+            filePath = str(output_file).replace(".mp4",".mp3")
+            os.remove(output_file)
+            LOG(f"[+] File \"{filePath}\" already exists")
+            Text_StatusText.config(text=Status.IDLE())
+            return
+        
+        # converting mp4 to mp3
+        base, ext = os.path.splitext(output_file)
+        new_file = base + '.mp3'
+        os.rename(output_file, new_file)
+
+        Text_StatusText.config(text=Status.IDLE())
+        LOG(f"[+] Succesfully downloaded: \"{new_file}\"")
+    except:
+        LOG("[+] Invalid Video Link")
+        Text_StatusText.config(text=Status.IDLE())
+
+def Download_Vid_To_Mp4():
+    global TextBox_youtube_VideoLink
+    global Text_StatusText
+    global TextBox_VideoPath
 
     try:
-        video = YouTube(videoLink)
+        Text_StatusText.config(text=Status.STARTING())
+        m_VideoLink = str(TextBox_youtube_VideoLink.get(1.0, "end-1c"))
+        m_VideoPath = str(TextBox_VideoPath.get(1.0, "end-1c"))
+
+        video = YouTube(m_VideoLink)
+        
+        stream = video.streams.get_highest_resolution()
+
+        Text_StatusText.config(text=Status.DOWNLOADING())
+        output_file = stream.download(m_VideoPath)
+        
+        LOG(f"[+] Succesfully downloaded: \"{output_file}\"")
+
+        Text_StatusText.config(text=Status.IDLE())
     except:
-        return
-    Text_StatusText.config(text="Status : Starting Download")
-    if (internetSpeed == 0):
-        sleep(2)
-        Text_StatusText.config(text="Status : Downloading")
-        pass
-    else:
-        Text_StatusText.config(text="Status : Downloading | "+str(internetSpeed)+ " Mbp/s")
-    stream = video.streams.get_highest_resolution()
-    stream.download()
-    Text_StatusText.config(text="Status : Idle")
-try:
-    if (str(argv[1]).lower() == "console"):
-        pass
-except:
-    Button_DownloadBtn = Button(width=25, height=5, text="DOWNLOAD",bg="#8FD7AC",command=multiThreadDownloadGUI)
+        LOG("[+] Invalid Video Link")
+        Text_StatusText.config(text=Status.IDLE())     
+
+def Multi_Threaded_MP3_Downloader():
+    downloader_Thread = threading.Thread(target=Download_Vid_To_Mp3)
+    downloader_Thread.start()
+
+def Multi_Threaded_MP4_Downloader():
+    downloader_Thread = threading.Thread(target=Download_Vid_To_Mp4)
+    downloader_Thread.start()
+
+def DOWNLOAD():
+    global CheckBox_Var
+    if CheckBox_Var.get() == 1:
+        Multi_Threaded_MP4_Downloader()
+    elif CheckBox_Var.get() == 0:
+        Multi_Threaded_MP3_Downloader()
+
+def Main():
+    global MAIN_WINDOW
+
+    # button used to download the youtube video
+    Button_DownloadBtn = Button(width=25, height=5, text="DOWNLOAD",bg="#8FD7AC",command=DOWNLOAD)
     Button_DownloadBtn.place(x=107, y=150)
 
-def mainGUI():
-    InternetSpeedThread()
-    #window properties
-    window.title(WINDOW_TITLE)
-    window.configure(width=WINDOW_WIDTH, height=WINDOW_HEIGHT,bg=WINDOW_BACKGROUND_COLOR)
-    window.resizable(False,False)
-    window.protocol("WM_DELETE_WINDOW", on_closing)
-    window.mainloop()
+    MAIN_WINDOW.mainloop()
 
-def mainConsole():
-    global videoRes
-    global videoLink
-    #InternetSpeedThread()
-    def Welcome():
-        system("cls")
-        print(f"[{PROGRAM_TITLE}]\n\n")
-    
-    Welcome()
-    videoLink = str(input("[+] Enter Video URL : "))
-
-    while (True):
-        videoRes = str(input("[+] Enter Video Resolution(low/high) : "))
-        if (videoRes.lower() == "low" or videoRes.lower() == "high"):
-            break
-
-    try:
-        video = YouTube(videoLink)
-    except:
-        print("[-] There was a problem")
-        _exit(1)
-
-    if (videoRes.lower() == "high"):
-        print("\n[*] Downloading in high resolution..")
-        stream = video.streams.get_highest_resolution()
-    elif(videoRes.lower() == "low"):
-        print("\n[*] Downloading in low resolution..")
-        stream = video.streams.get_lowest_resolution()
-    
-
-    stream.download()
-
-    print(f"\n[+] \"{video.title}\" downloaded successfully in: {pathToProgram}")
-
-
-# handling ctrl+c
-signal.signal(signal.SIGINT, OnExithandler)
 
 if __name__ == "__main__":
-    try:
-        if (str(argv[1]).lower() == "console"):
-            mainConsole()
-            on_closing("console")
-    except:
-        mainGUI()
+    LOG(f"[+] [Thanks for using \"{APPLICATION_NAME}\"]\n")
+    GUI_PROPERTIES()
+    GUI_THREAD = threading.Thread(target=Main()).start()
